95b72441c1502d9b9f7632bbe19263dc59532d2d
PixPixLol2 <yayitstoday22@gmail.com>
2024-09-24 22:39:36 -0500

    update

--- calculator.py
+++ calculator.py
@@ -5,28 +5,33 @@ import tkinter as tk
     
 canvas = tk.Tk()
 
-def enterNumber(num):
+
+
+
+    
+
+def ENTER_NUMBER(num):
     entry1.insert(tk.END,str(num))
 
-def enterNumberOne():
+def ENTER_NUMBEROne():
     entry1.insert(tk.END,"1")
-def enterNumberTwo():
+def ENTER_NUMBERTwo():
     entry1.insert(tk.END,"2")
-def enterNumberThree():
+def ENTER_NUMBERThree():
     entry1.insert(tk.END,"3")
-def enterNumberFour():
+def ENTER_NUMBERFour():
     entry1.insert(tk.END,"4")
-def enterNumberFive():
+def ENTER_NUMBERFive():
     entry1.insert(tk.END,"5")
-def enterNumberSix():
+def ENTER_NUMBERSix():
     entry1.insert(tk.END,"6")
-def enterNumberSeven():
+def ENTER_NUMBERSeven():
     entry1.insert(tk.END,"7")
-def enterNumberEight():
+def ENTER_NUMBEREight():
     entry1.insert(tk.END,"8")
-def enterNumberNine():
+def ENTER_NUMBERNine():
     entry1.insert(tk.END,"9")
-def enterNumberZero():
+def ENTER_NUMBERZero():
     entry1.insert(tk.END,"0")
 def enterOperationMinus():
     entry1.insert(tk.END, "-")
@@ -47,46 +52,46 @@ def clearInputBox():
     
 entry1 = tk.Entry(canvas)
 entry1.grid()
-button1 = tk.Button(canvas,text="1",command=lambda: enterNumber(1))
+button1 = tk.Button(canvas,text="1", command=lambda: ENTER_NUMBER(1))
 button1.grid(row=1,column=0, sticky="NEWS")
-button2 = tk.Button(canvas,text="2",command=lambda: enterNumber(2))
+button2 = tk.Button(canvas,text="2",command=lambda: ENTER_NUMBER(2))
 button2.grid(row=1,column=1, sticky="NEWS")
-button3 = tk.Button(canvas,text="3",command=lambda: enterNumber(3))
+button3 = tk.Button(canvas,text="3",command=lambda: ENTER_NUMBER(3))
 button3.grid(row=1,column=2, sticky="NEWS")
-button4 = tk.Button(canvas,text="4",command=lambda: enterNumber(4))
+button4 = tk.Button(canvas,text="4",command=lambda: ENTER_NUMBER(4))
 button4.grid(row=2,column=0, sticky="NEWS")
-button5 = tk.Button(canvas,text="5",command=lambda: enterNumber(5))
+button5 = tk.Button(canvas,text="5",command=lambda: ENTER_NUMBER(5))
 button5.grid(row=2,column=1, sticky="NEWS")
-button6 = tk.Button(canvas,text="6",command=lambda: enterNumber(6))
+button6 = tk.Button(canvas,text="6",command=lambda: ENTER_NUMBER(6))
 button6.grid(row=2,column=2, sticky="NEWS")
-button7 = tk.Button(canvas,text="7",command=lambda: enterNumber(7))
+button7 = tk.Button(canvas,text="7",command=lambda: ENTER_NUMBER(7))
 button7.grid(row=3,column=0, sticky="NEWS")
-button8 = tk.Button(canvas,text="8", command=lambda: enterNumber(8))
+button8 = tk.Button(canvas,text="8", command=lambda: ENTER_NUMBER(8))
 button8.grid(row=3, column=1, sticky="NEWS")
-button9 = tk.Button(canvas,text="9", command=lambda: enterNumber(9))
+button9 = tk.Button(canvas,text="9", command=lambda: ENTER_NUMBER(9))
 button9.grid(row=3, column=2, sticky="NEWS")
-button0 = tk.Button(canvas,text="0", command=lambda: enterNumber(0))
-button0.grid(row=3, column=3, sticky="NEWS")
-button_equal = tk.Button(canvas,text="=", command=enterOperationEqual)
+button0 = tk.Button(canvas,text="0", command=lambda: ENTER_NUMBER(0))
+button0.grid(row=4, column=0, sticky="NEWS")
+button_equal = tk.Button(canvas,text="=", bg="red", command=enterOperationEqual)
 button_equal.grid(row=5,column=2, sticky="NEWS")
-button_plus = tk.Button(canvas,text="+", command=enterOperationPlus)
+button_plus = tk.Button(canvas,text="+", bg="red", command=enterOperationPlus)
 button_plus.grid(row=1,column=3, sticky="NEWS")
-buttonMinus = tk.Button(canvas,text="-", command=enterOperationMinus)
-buttonMinus.grid(row=2, column=3, sticky="NEWS")
-buttonTimes = tk.Button(canvas,text="*", command=enterOperationTimes)
-buttonTimes.grid(row=3, column=3, sticky="NEWS")
-buttonDivision = tk.Button(canvas,text="/", command=enterOperationDivision)
-buttonDivision.grid(row=4, column=3, sticky="NEWS")
-buttonClear = tk.Button(canvas,text="C", command=clearInputBox)
-buttonClear.grid(row=4, column=0, sticky="NEWS")
+buttonMinus = tk.Button(canvas,text="-", bg="red", command=enterOperationMinus)
+buttonMinus.grid(row=4, column=3, sticky="NEWS")
+buttonTimes = tk.Button(canvas,text="*", bg="red", command=enterOperationTimes)
+buttonTimes.grid(row=2, column=3, sticky="NEWS")
+buttonDivision = tk.Button(canvas,text="/", bg="red", command=enterOperationDivision)
+buttonDivision.grid(row=3, column=3, sticky="NEWS")
+buttonClear = tk.Button(canvas,text="C", bg="orange", command=clearInputBox)
+buttonClear.grid(row=1, column=3, sticky="NEWS")
 
 
 entry1.grid(row=0,column=0,columnspan=4)
 button1.grid(row=1,sticky="NEWS")
 button2.grid(row=1,column=1, sticky="NEWS")
 button_equal.grid(row=1,column=2, sticky="NEWS")
-button_equal.grid(row=1,column=3, sticky="NEWS")
-button_plus.grid(row=1, column=2, sticky="NEWS")
+button_equal.grid(row=4,column=2, sticky="NEWS")
+button_plus.grid(row=4, column=1, sticky="NEWS")
 
 
 canvas.mainloop()
